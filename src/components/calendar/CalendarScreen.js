3be772e3953ffbc83da41ef55c5a68e02f3c3b58
Ernesto Emmanuel Gutierrez Muñoz <ernesto.gtz@itcoach.com.mx>
2020-08-28 08:48:59 -0500

    continue with project

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,54 @@
 import React from 'react';
 import { Navbar } from '../ui/Navbar';
 
+import {Calendar,momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
+
+import { messages } from '../../helpers/calendar-messages';
+
+moment.locale('es');
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+    title:'Cumpleanos del jefe',
+    start: moment().toDate(),
+    end:moment().add(2,'hours').toDate(),
+    bgcolor:'#fafafa',
+    notes:'Comprar el pastel'
+}];
+
 export const CalendarScreen = () => {
+    
+    const eventStyleGetter = (event,start,end,isSelected) =>{
+        //console.log(event,start,end,isSelected);
+        const style = {
+            backgroundColor:'#367CF7',
+            borderRadius:'0px',
+            opacity:0.8,
+            display:'block',
+            color:'white'
+        }
+        return {
+            style
+        }
+    }
+    
     return (
-        <div>
+        <div className="calendar-screen">
             <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                messages={messages}
+                eventPropGetter={eventStyleGetter}
+            />
         </div>
     )
 }
